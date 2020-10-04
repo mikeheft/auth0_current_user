212709e20dc998b07c176d8091af60dc5f7d3cf7
Mike Heft <mikeheft@gmail.com>
2020-10-04 13:20:34 -0600

    Work on template path issue

--- lib/generators/auth0_current_user/install_generator.rb
+++ lib/generators/auth0_current_user/install_generator.rb
@@ -2,7 +2,7 @@ require 'rails/generators/base'
 
 module Auth0CurrentUser
   class InstallGenerator < Rails::Generators::Base
-    source_root File.expand_path("../templates", __FILE__ )
+    source_root File.expand_path('../../templates', __FILE__ )
 
     def self.exit_on_failure
       true

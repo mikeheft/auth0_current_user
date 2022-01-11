df7c5933fead631e6aed69cb40a05a568c90ede7
Mike Heft <mikeheft@gmail.com>
2022-01-11 06:36:52 -0700

    Add client id to configuration to facilitate login redirects

--- lib/auth0_current_user/configuration.rb
+++ lib/auth0_current_user/configuration.rb
@@ -1,11 +1,12 @@
 module Auth0CurrentUser
   class Configuration
-    attr_accessor :auth0_domain, :auth0_audience, :authenticated_klass
+    attr_accessor :auth0_domain, :auth0_audience, :authenticated_klass, :client_id
 
     def initialize
       @auth0_domain = nil
       @auth0_audience = nil
       @authenticated_klass = :user
+      @client_id = nil
     end
     
   end
